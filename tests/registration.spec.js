0304a6478f52048455419898abbff23fe3d13d06
Sergey Balandin <sergblainf@yandex.ru>
2025-05-22 23:17:48 +0300

    feat(test): add registration test and example Playwright tests
    This commit introduces a new registration test in `registration.spec.js` and adds example Playwright tests in `example.spec.js` and `demo-todo-app.spec.js`. Additionally, it updates the repository URL in `package.json`, adds the `@faker-js/faker` dependency for test data generation, and includes a new GitHub Actions workflow for running Playwright tests.

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -1,6 +1,9 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
-test('Регистрация', async ({page})  => {
-  await page.goto('https://www.demoblaze.com/');
-})
+test("Регистрация", async ({ page }) => {
+  await page.goto("https://www.demoblaze.com/");
+
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole('textbox', {name: 'Username'}).fill('Serget');
+});
